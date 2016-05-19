49b2c541c09065ea8a3aa4973526aa7cd10ea13e
Flynn <flynn@kodachi.com>
2016-05-19 13:24:00 -0400

    Stupid unzip is stupid.

--- pkgconf.sh
+++ pkgconf.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-# Test pkgconf script
+set -e
+set -o pipefail
 
 source="$1"; shift
 dest="$1"; shift
@@ -16,6 +17,13 @@ if [ $VERBOSITY -gt 2 ]; then
 fi
 
 if [ "$phase" == "postinstall" ]; then
+	# Stupid unzip is stupid.
+	if [ ! -d "${dest}/templates" ]; then
+		mkdir "${dest}/templates"
+	fi
+
+	mv "${dest}/basic.sh" "${dest}/templates"
+
 	rm -f "${dest}/Makefile"
 	rm -f "${dest}/install.sh"
 	rm -f "${dest}/pkgconf.sh"
