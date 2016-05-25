4dc2f8c8155fb7411f35f5bfcf02c68e98249aad
Flynn <flynn@kodachi.com>
2016-05-25 17:35:07 -0400

    Support - but do not require - installing Python into the current virtualenv.

--- Makefile
+++ Makefile
@@ -1,12 +1,18 @@
 all: install.sh
 
-install.sh: make_installer.py templates/basic.sh
+install.sh: checkEnv pip make_installer.py templates/basic.sh
 	python make_installer.py \
 		--template=templates/basic.sh \
 		utilities \
 		https://github.com/datawire/utilities/archive/master.zip \
 		utilities \
 		> install.sh
+
+checkEnv:
+	@if [ -z "$$VIRTUAL_ENV" ]; then echo "You must be in a venv for this"; false; fi
+
+pip: checkEnv
+	pip install -q -r requirements.txt	# for dev
 
 clean:
 	rm -f install.sh
--- modules/install-python.sh
+++ modules/install-python.sh
@@ -1,17 +1,28 @@
-#### Module to install Python files
 do_installation () {
     source="${1}"
     target="${2}"
+    support_existing_venv="${3}"
 
-    step "Creating installation directory..."
+    if [ -n "${support_existing_venv}" -a \( "${target}" = "venv" \) ]; then
+        if [ -z "${VIRTUAL_ENV}" ]; then
+            echo "Target 'venv' only makes sense in a virtualenv" >&2
+            exit 1
+        fi
 
-    if [ ! -d "${install_destination}" ]; then
-        mkdir -p "${install_destination}"
+        # RE-activate the existing venv. I'm not sure why my setup clobbers
+        # the venv's PATH setting, but it does...
+        . ${VIRTUAL_ENV}/bin/activate
+    else
+        step "Creating installation directory..."
+
+        if [ ! -d "${install_destination}" ]; then
+            mkdir -p "${install_destination}"
+        fi
+
+        virtualenv -q --python python2.7 "${install_destination}/venv"
+
+        . ${install_destination}/venv/bin/activate
     fi
-
-    virtualenv -q --python python2.7 "${install_destination}/venv"
-
-    . ${install_destination}/venv/bin/activate
 
     step "Installing..."
 
@@ -25,7 +36,9 @@ do_installation () {
 
     run_script pkgconf.sh "${source}" "${target}" "postinstall"
 
-    deactivate
+    if [ -z "${support_existing_venv}" -o \( "${target}" != "venv" \) ]; then
+        deactivate
+    fi
 
     step "Installed!"
 }
