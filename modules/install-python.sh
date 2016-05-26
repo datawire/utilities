3d37342411eba7976f6d127f4fd24272797c3dd2
Flynn <flynn@kodachi.com>
2016-05-26 15:18:49 -0400

    Support using the system Python installation, too (needed on e.g. AMIs).

--- modules/install-python.sh
+++ modules/install-python.sh
@@ -1,18 +1,17 @@
 do_installation () {
     source="${1}"
     target="${2}"
-    support_existing_venv="${3}"
+    support_existing="${3}"
 
-    if [ -n "${support_existing_venv}" -a \( "${target}" = "venv" \) ]; then
-        if [ -z "${VIRTUAL_ENV}" ]; then
-            echo "Target 'venv' only makes sense in a virtualenv" >&2
-            exit 1
+    create_venv=YES
+
+    if [ -n "${support_existing}" ]; then
+        if [ \( "${target}" = "venv" \) -o \( "${target}" = "system" \) ]; then
+            create_venv=""
         fi
+    fi
 
-        # RE-activate the existing venv. I'm not sure why my setup clobbers
-        # the venv's PATH setting, but it does...
-        . ${VIRTUAL_ENV}/bin/activate
-    else
+    if [ -n "${create_venv}" ]; then
         step "Creating installation directory..."
 
         if [ ! -d "${install_destination}" ]; then
@@ -22,6 +21,18 @@ do_installation () {
         virtualenv -q --python python2.7 "${install_destination}/venv"
 
         . ${install_destination}/venv/bin/activate
+    elif [ "${target}" = "venv" ]; then
+        if [ -z "${VIRTUAL_ENV}" ]; then
+            echo "Target 'venv' only makes sense in a virtualenv" >&2
+            exit 1
+        fi
+
+        # RE-activate the existing venv. I'm not sure why my setup clobbers
+        # the venv's PATH setting, but it does...
+        . ${VIRTUAL_ENV}/bin/activate
+    elif [ "${target}" != "system" ]; then
+        echo "Impossible: create_venv false but target is ${target}" >&2
+        exit 1
     fi
 
     step "Installing..."
@@ -36,7 +47,7 @@ do_installation () {
 
     run_script pkgconf.sh "${source}" "${target}" "postinstall"
 
-    if [ -z "${support_existing_venv}" -o \( "${target}" != "venv" \) ]; then
+    if [ -n "${create_venv}" ]; then
         deactivate
     fi
 
